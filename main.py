f78084ad570ec1adfd37af1cb5a32f31dea3318e
syab2 <kolakrylov6@gmail.com>
2022-03-24 15:00:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, current_user
+
+from data import db_session
+from forms.register import RegisterForm
+from data.users import User
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def base():
+    return render_template('index.html', current_user=current_user)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.nickname = form.nickname.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_session.global_init('db/mega.db')
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
