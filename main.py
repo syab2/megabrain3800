b6d65bd2a0af6482bd5663fb73191dbe00c2eaf0
syab2 <kolakrylov6@gmail.com>
2022-03-26 11:34:33 +0300

    login and registration completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
+from forms.login import LoginForm
 from data import db_session
 from forms.register import RegisterForm
 from data.users import User
@@ -17,8 +18,15 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
 @app.route('/')
-def base():
+def index():
     return render_template('index.html', current_user=current_user)
 
 
@@ -43,6 +51,21 @@ def register():
         db_sess.commit()
         return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 def main():
